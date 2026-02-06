7826414a4c858ab6656e164f4a0fe23b81d181ec
thesparrow99 <guyutampan@gmail.com>
2026-02-06 22:37:41 +0700

    week01: remove explicit types to use type inference

--- src/oop_00000130569_MuhamadZhakyAlamsyah/Week_01/CircleCalculator.kt
+++ src/oop_00000130569_MuhamadZhakyAlamsyah/Week_01/CircleCalculator.kt
@@ -1,18 +1,17 @@
-package oop_00000130569_MuhamadZhakyAlamsyah.Week_01
+package oop_00000130569_muhamadzhakyalamsyah.week01
 
-fun main(args: Array<String>) {
-    var radius: Double = 7.0
-    var pi: Double = 3.14
-    var area: Double = pi * radius * radius
+fun main() {
+    // REFACTOR: Tipe data : Double dihapus karena Kotlin sudah tahu 7.0 adalah Double
+    val radius = 7.0
+    val pi = 3.14
+    val area = pi * radius * radius
 
-    println("Radius: " + radius + ", Area: " + area)
+    println("Radius: $radius, Area: $area")
+
     checkSize(area)
 }
 
-fun checkSize(area: Double) {
-    if (area > 100) {
-        println("This is a Big Circle")
-    } else {
-        println("This is a Small Circle")
-    }
+fun checkSize(area: Double) = when {
+    area > 100 -> println("This is a Big Circle")
+    else -> println("This is a Small Circle")
 }
