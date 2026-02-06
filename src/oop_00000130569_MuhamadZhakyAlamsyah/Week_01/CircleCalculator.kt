c8d75a7740dd16cdec8074263c8ebac389961359
thesparrow99 <guyutampan@gmail.com>
2026-02-06 22:42:49 +0700

    week01: (tugas 1) convert checkSize to expression body function

--- src/oop_00000130569_MuhamadZhakyAlamsyah/Week_01/CircleCalculator.kt
+++ src/oop_00000130569_MuhamadZhakyAlamsyah/Week_01/CircleCalculator.kt
@@ -1,17 +1,13 @@
 package oop_00000130569_muhamadzhakyalamsyah.week01
 
 fun main() {
-    // REFACTOR: Tipe data : Double dihapus karena Kotlin sudah tahu 7.0 adalah Double
     val radius = 7.0
     val pi = 3.14
     val area = pi * radius * radius
 
     println("Radius: $radius, Area: $area")
 
-    checkSize(area)
+    println(checkSize(area))
 }
 
-fun checkSize(area: Double) = when {
-    area > 100 -> println("This is a Big Circle")
-    else -> println("This is a Small Circle")
-}
+fun checkSize(area: Double) = if (area > 100) "This is a Big Circle" else "This is a Small Circle"
