1a8933ea0f4b2bb5d4685995c52b04afc64c0555
thesparrow99 <guyutampan@gmail.com>
2026-02-06 21:54:39 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000130569_MuhamadZhakyAlamsyah/Week_01/GradingSystem.kt
@@ -0,0 +1,7 @@
+package oop_00000130569_MuhamadZhakyAlamsyah.Week_01
+
+fun main() {
+    var name : String = "Muhamad Zhaky Alamsyah"
+    var score: Int = 90
+    println("Nama: " + name + ", Nilai: " + score)
+}
