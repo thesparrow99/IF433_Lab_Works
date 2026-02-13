1119d8b1c476623cededea6b8a64f26e68468327
thesparrow99 <guyutampan@gmail.com>
2026-02-13 11:34:11 +0700

    week02: create basic Student class structure

--- src/oop_000000130569_MuhamadZhakyAlamsyah_Week2/Student.kt
+++ src/oop_000000130569_MuhamadZhakyAlamsyah_Week2/Student.kt
@@ -1,4 +1,18 @@
 package oop_000000130569_MuhamadZhakyAlamsyah_Week2
 
-class Student {
+class Student (
+    val name : String,
+    val nim : String,
+    val major : String
+)
+
+class Student (val name : String, val nim : String, val major : String){
+    init {
+        if (nim.length !=5) {
+            println("WARNING:Objek tercipta dengan NIM ($nim) yang valid!")
+            println("Data mmahasiswa $name mungkin akan bermasalah di sistem.")
+        }else {
+            println("LOG: Objek Student $name berhasil dialokasikan di memory"
+        }
+    }
 }
--- src/oop_00000130569_MuhamadZhakyAlamsyah/Week_01/GradingSystem.kt
+++ src/oop_00000130569_MuhamadZhakyAlamsyah/Week_01/GradingSystem.kt
@@ -1,31 +1,24 @@
-package oop_00000130569_MuhamadZhakyAlamsyah.Week01.
-
-fun main() {
-    var name : String = "Muhamad Zhaky Alamsyah"
-    var score: Int = 90
-    println("Nama: " + name + ", Nilai: " + score)
-}
+package oop_00000130569_muhamadzhakyalamsyah.week01
 
 fun main() {
     val name = "Muhamad Zhaky Alamsyah"
     val score = 90
 
     println("Nama: $name, Nilai: $score")
+
     val studentId: String? = null
     val idLength = studentId?.length ?: 0
     println("Panjang ID: $idLength")
-}
 
-val grade = when (score) {
-    in 90..100 -> "A"
-    in 80..89 -> "B"
-    in 70..79 -> "C"
-    else -> "D"
-}
+    val grade = when (score) {
+        in 90..100 -> "A"
+        in 80..89 -> "B"
+        in 70..79 -> "C"
+        else -> "D"
+    }
 
-println("Grade kamu: $grade")
-
-println("Status: ${calculateStatus(score)}")
+    println("Grade kamu: $grade")
+    println("Status: ${calculateStatus(score)}")
 }
 
 fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
