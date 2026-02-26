14e864b65f77dacaf249cbb8268da7d560a57dea
thesparrow99 <guyutampan@gmail.com>
2026-02-26 21:06:08 +0700

    week04: create Car class extending Vehicle

---
+++ src/oop_00000130569_MuhamadZhakyAlamsyah/Week_04/Car.kt
@@ -0,0 +1,8 @@
+package oop_00000130569_MuhamadZhakyAlamsyah.Week_04
+
+open class Car(brand: String, val numberOfDoors: Int) : Vehicle(brand)  {
+
+    fun openTrunk(){
+        println("Bagasi mobil $brand dengan $numberOfDoors pintu dibuka.")
+    }
+}
