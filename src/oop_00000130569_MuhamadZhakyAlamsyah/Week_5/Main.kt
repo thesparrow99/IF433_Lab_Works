86675c0a2a15602f902e35e8625df059115e899b
thesparrow99 <guyutampan@gmail.com>
2026-03-06 12:46:39 +0700

    week05: (task 2) implement payment polymorphism

--- src/oop_00000130569_MuhamadZhakyAlamsyah/Week_5/Main.kt
+++ src/oop_00000130569_MuhamadZhakyAlamsyah/Week_5/Main.kt
@@ -31,4 +31,48 @@ fun main() {
         println("Luas Persegi Panjang (10x5): ${helper.hitungLuas(10, 5)}")
         println("Luas Lingkaran (r=7.0): ${helper.hitungLuas(7.0)}")
     }
+    fun main() {
+        val myEWallet = EWallet(accountName = "Zhaky E-Wallet", balance = 50000.0)
+        val myCreditCard = CreditCard(accountName = "Zhaky Visa", limit = 100000.0)
+
+        val listPembayaran: List<PaymentMethod> = listOf(myEWallet, myCreditCard)
+
+        println("=== PROSES PEMBAYARAN SEJUMLAH 75.000 ===")
+
+        for (metode in listPembayaran) {
+            metode.processPayment(75000.0)
+
+            when (metode) {
+                is EWallet -> {
+                    println("Info: Saldo E-Wallet saat ini adalah ${metode.balance}")
+                    metode.topUp(25000.0)
+                }
+                is CreditCard -> {
+                    println("Info: Sisa limit kartu adalah ${metode.limit - metode.usedAmount}")
+                }
+            }
+            println("------------------------------------------")
+        }
+
+        val dosen1 = Dosen(nama = "Pak Theo", nidn = "0123456")
+        val dosen2 = Dosen(nama = "Bu Siti", nidn = "0000000")
+        val admin1 = Admin(nama = "Budi")
+
+        val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1)
+
+        println("\n=== AKTIFITAS PEGAWAI ===")
+        for (pegawai in daftarPegawai) {
+            pegawai.bekerja()
+            when (pegawai) {
+                is Admin -> {
+                    println("=> Terdeteksi sebagai Admin")
+                    pegawai.doAdminWork()
+                }
+                is Dosen -> {
+                    println("=> Terdeteksi sebagai Dosen")
+                }
+            }
+            println("------------------")
+        }
+    }
 }
