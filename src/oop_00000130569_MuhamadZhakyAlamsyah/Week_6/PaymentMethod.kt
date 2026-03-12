a75f4808389004a9ae5233630c75e62065518a45
thesparrow99 <guyutampan@gmail.com>
2026-03-13 01:06:55 +0700

    week06: create Gopay and CreditCard

--- src/oop_00000130569_MuhamadZhakyAlamsyah/Week_6/PaymentMethod.kt
+++ src/oop_00000130569_MuhamadZhakyAlamsyah/Week_6/PaymentMethod.kt
@@ -1,5 +1,10 @@
 package oop_00000130569_MuhamadZhakyAlamsyah.Week_6
 
 interface PaymentMethod  {
-    fun pay(amount: Double)
+    fun pay(amount: Double) { println("Processing $amount via Gopay Server.") }
+}
+
+class CreditCard : PaymentMethod {
+    override fun pay(amount: Double) { println("Contacting Bank for Rp$amount") }
+    }
 }
