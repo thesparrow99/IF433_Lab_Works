69a31e256fd87a63f07b117118711091fb49ec6e
thesparrow99 <guyutampan@gmail.com>
2026-03-13 01:17:25 +0700

    week06: (task) create smart home interfaces

---
+++ src/oop_00000130569_MuhamadZhakyAlamsyah/Week_6/SmartInterfaces.kt
@@ -0,0 +1,19 @@
+package oop_00000130569_MuhamadZhakyAlamsyah.Week_6
+
+interface SmartDevice {
+    val id: String
+    val name: String
+}
+
+interface Switchable {
+    fun turnOn()
+    fun turnOff()
+}
+
+interface Recordable {
+    fun startRecord()
+
+    fun stopRecord() {
+        println("Perekaman dihentikan dan disimpan ke Cloud.")
+    }
+}
