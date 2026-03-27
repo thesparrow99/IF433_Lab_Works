395628c0722984f58e0242c240c5977ed1caf056
thesparrow99 <guyutampan@gmail.com>
2026-03-27 15:39:28 +0700

    week07: (task) test data class copy and sealed class event dispatch

--- src/oop_00000130569_MuhamadZhakyAlamsyah/Week_07/Main.kt
+++ src/oop_00000130569_MuhamadZhakyAlamsyah/Week_07/Main.kt
@@ -43,4 +43,13 @@ fun main() {
 
     val starterWeapon = Weapon.forgeStarterSword()
     println("Senjata Awal: ${starterWeapon.item.name} | Damage: ${starterWeapon.item.damage}")
+
+    println("\n=== TEST DATA CLASS COPY & EVENT DISPATCH ===")
+
+    val upgradedItem = starterWeapon.item.copy(name = "Pedang Kayu Sakti", damage = 25)
+
+    processEvent(BattleState.SafeZone)
+    processEvent(BattleState.MonsterEncounter("Goblin Nakal"))
+    processEvent(BattleState.LootDropped(upgradedItem))
+    processEvent(BattleState.GameOver("Terkena jebakan racun"))
 }
