80746d222100f3ee4a94eb146218b44c86639215
thesparrow99 <guyutampan@gmail.com>
2026-04-16 22:19:57 +0700

    week08: implement java interop using !!

--- src/oop_00000130569_MuhamadZhakyAlamsyah/Week_08/Main.kt
+++ src/oop_00000130569_MuhamadZhakyAlamsyah/Week_08/Main.kt
@@ -54,4 +54,10 @@ fun main() {
     } catch (e: IllegalArgumentException) {
         println(e.message)
     }
+
+    println("\n=== TEST JAVA INTEROP ===")
+    val javaResponse = LegacyJavaAPI.fetchServerStatus()
+
+    val statusLength = javaResponse!!.length
+    println("Status dari Java: $javaResponse (Length: $statusLength)")
 }
