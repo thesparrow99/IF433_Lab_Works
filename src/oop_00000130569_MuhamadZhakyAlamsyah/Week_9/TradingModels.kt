5e73e9d9bb456064f86bf5ec903f46baf82d8294
thesparrow99 <guyutampan@gmail.com>
2026-04-24 10:38:09 +0700

    week09: (task) create TradeLog data class

---
+++ src/oop_00000130569_MuhamadZhakyAlamsyah/Week_9/TradingModels.kt
@@ -0,0 +1,9 @@
+package oop_00000130569_MuhamadZhakyAlamsyah.Week_9
+
+data class TradeLog(
+    val pair: String,
+    val position: String,
+    val leverage: Int,
+    val roe: Double,
+    val status: String
+)
