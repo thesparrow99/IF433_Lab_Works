7697267818aa27062592f35e5d7e7aa181e923ed
thesparrow99 <guyutampan@gmail.com>
2026-05-08 12:09:25 +0700

    week11: (task) create SmartDevice data class

---
+++ src/oop_00000130569_MuhamadZhakyAlamsyah/Week_11/SmartHomeModels.kt
@@ -0,0 +1,8 @@
+package oop_00000130569_MuhamadZhakyAlamsyah.Week_11
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
