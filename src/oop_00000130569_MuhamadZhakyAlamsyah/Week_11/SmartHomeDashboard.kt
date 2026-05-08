5bebcdaeb8eddecada23b0a766cd936dcbccdb20
thesparrow99 <guyutampan@gmail.com>
2026-05-08 12:15:21 +0700

    week11: (task) implement device search with let

--- src/oop_00000130569_MuhamadZhakyAlamsyah/Week_11/SmartHomeDashboard.kt
+++ src/oop_00000130569_MuhamadZhakyAlamsyah/Week_11/SmartHomeDashboard.kt
@@ -21,4 +21,12 @@ fun main() {
         val device = SmartDevice("Daikin Inverter (Kabel 3x2.5)", "HVAC", false, 800)
         device // Menghasilkan (return) objek tersebut
     }
+    homeDevices.add(acUnit)
+    homeDevices.add(SmartDevice("Picolo's Auto Feeder", "Pet Care", true, 10))
+
+    val searchResult = homeDevices.find { it.category == "Camera" }
+    searchResult?.let {
+        println("\n--- Hasil Pencarian Kamera ---")
+        println(it.diagnose())
+    }
 }
