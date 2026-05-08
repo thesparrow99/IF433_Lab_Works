7cc65cf53e9c90e80ed307ed47703ca73df0d153
thesparrow99 <guyutampan@gmail.com>
2026-05-08 11:41:05 +0700

    week11: create string extension functions

---
+++ src/oop_00000130569_MuhamadZhakyAlamsyah/Week_11/StringExtensions.kt
@@ -0,0 +1,9 @@
+package oop_00000130569_MuhamadZhakyAlamsyah.Week_11
+
+fun String.addGreating() : String {
+    return "Hallo, $this"
+}
+
+fun String.repostTimes(n: Int): String {
+    return this.repeat(n)
+}
