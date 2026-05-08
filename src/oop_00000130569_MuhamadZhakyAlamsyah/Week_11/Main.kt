2bcf485dc9598e40e7765a331c22c956979bd994
thesparrow99 <guyutampan@gmail.com>
2026-05-08 11:46:04 +0700

    week11: test extension functions in main

---
+++ src/oop_00000130569_MuhamadZhakyAlamsyah/Week_11/Main.kt
@@ -0,0 +1,10 @@
+package oop_00000130569_MuhamadZhakyAlamsyah.Week_11
+
+fun main(){
+    println("=== TEST EXTENTION FUNCTION ===")
+    println("Alex". addGreating())
+    println("Hi". repostTimes(3))
+
+    val text: String? = null
+    println("Apakah null/empty? ${text.isnullOfEmptyCustom()}")
+}
