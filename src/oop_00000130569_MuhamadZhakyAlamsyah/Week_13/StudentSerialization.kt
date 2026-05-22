6ea0b020b253616517d88f7bb0dc604b9bc327a8
thesparrow99 <guyutampan@gmail.com>
2026-05-22 09:45:59 +0700

    week13: create Student model

---
+++ src/oop_00000130569_MuhamadZhakyAlamsyah/Week_13/StudentSerialization.kt
@@ -0,0 +1,6 @@
+package oop_00000130569_MuhamadZhakyAlamsyah.Week_13
+
+import java.io.File
+import java.io.FileNotFoundException
+
+data class Student(val name: String, val age: Int, val gpa: Double)
